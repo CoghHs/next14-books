3dda9f3d1da006f665bb5e8706700209ff826c62
박형석 <baghyeongseog@baghyeongseog-ui-MacBookAir.local>
2025-06-11 16:00:42 +0900

    css

--- app/about-us/layout.tsx
+++ app/about-us/layout.tsx
@@ -1,10 +1,12 @@
+import styles from "../../styles/about.module.css";
+
 export default function AboutUsLayOut({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
+    <div className={styles.space}>
       {children}
       &copy; The New York Times Best Seller Explorer
     </div>
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,10 +5,12 @@ export default function AboutUs() {
     <div className={styles.info}>
       <h1>About Us</h1>
       <h2>
-        Welcome to the official explorer for The New York Times Best Seller list
-        explorer .
+        뉴욕타임즈 베스트셀러 목록을 한눈에 확인하고,
+        <br />
+        Amazon 구매 페이지로 이동할 수 있는 간단한 책 탐색 웹앱입니다.
+        <br />
+        Next.js 14 기반으로, 카테고리별 도서를 한눈에 볼 수 있도록 구성했습니다.
       </h2>
-      <h2>We hope you enjoy your stay !</h2>
     </div>
   );
 }
