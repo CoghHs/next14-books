4a0752dd1f5a3f986a42b52e9ab3d2caf760bb32
박형석 <baghyeongseog@baghyeongseog-ui-MacBookAir.local>
2025-06-11 16:12:22 +0900

    booklist type

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,7 +8,10 @@ export const metadata = {
 };
 
 export default async function Home() {
-  const books: BookList[] = await getBookLists();
+  const books: BookList[] | undefined = await getBookLists();
+  if (!books || books.length === 0) {
+    return <div>책 정보를 불러올 수 없습니다.</div>;
+  }
   return (
     <div className={styles.list}>
       {books.map((i) => (
